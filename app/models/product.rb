39b5128b9176af2256683526f79212c340f24b32
Nathan <nathan.fenimore@gmail.com>
2018-11-01 09:14:44 +1000

    minor changes prior to deploy

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,11 @@
 class Product < ApplicationRecord
   belongs_to :user
-<<<<<<< HEAD
+
+  has_one_attached :image
 
   # validates if a user has authorisation to make changes
   def can_change?(user)
     self.user == user || user.has_role?(:admin)
   end
-=======
-  has_one_attached :image
->>>>>>> f797ef645916ee1eefa39a10b5b28c628e214102
+  
 end

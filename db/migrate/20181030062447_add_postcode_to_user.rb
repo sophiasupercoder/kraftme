233b1921f3dff866c89c0c4489f7e1f3704d9678
Mark Conanan <mark.conanan@gmail.com>
2018-10-31 11:54:05 +1000

    Product and Profile tables

---
+++ db/migrate/20181030062447_add_postcode_to_user.rb
@@ -0,0 +1,5 @@
+class AddPostcodeToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :postcode, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_29_063442) do
+ActiveRecord::Schema.define(version: 2018_10_30_062503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "product_title"
+    t.text "description"
+    t.decimal "price"
+    t.string "medium"
+    t.integer "quantity"
+    t.string "creator"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +36,16 @@ ActiveRecord::Schema.define(version: 2018_10_29_063442) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "address"
+    t.string "city"
+    t.integer "postcode"
+    t.string "state"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "products", "users"
 end

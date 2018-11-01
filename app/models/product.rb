718d9d7b435372f579d608ef85e11f0667cf7c4a
beth94victoria <beth.callanan@gmail.com>
2018-11-01 16:50:46 +1000

    added art_works folder to fixtures

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,9 @@
 class Product < ApplicationRecord
   belongs_to :user
 
-  has_one_attached :image
-
   # validates if a user has authorisation to make changes
   def can_change?(user)
     self.user == user || user.has_role?(:admin)
   end
-  
+  has_one_attached :image
 end

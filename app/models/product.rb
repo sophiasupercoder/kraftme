24b84ecd1c51263c23d97ed00b7fd4825535eecf
sophia <sophia.zhu10@gmail.com>
2018-11-01 13:19:24 +1000

    product validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,12 @@ class Product < ApplicationRecord
 
   has_one_attached :image
 
+  validates :product_title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :medium, presence: true
+  validates :quantity, presence: true
+  validates :creator, presence: true
   # validates if a user has authorisation to make changes
   def can_change?(user)
     self.user == user || user.has_role?(:admin)

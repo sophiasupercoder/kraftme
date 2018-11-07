6d406b574640c2358d749b7deebac5a93cb94929
Mark Angelo Conanan <mark.conanan@gmail.com>
2018-11-07 15:39:30 +1000

    Added validation on quantity and admin, changed headings and other cosmetic changes

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   get 'home/index'
   # route for user products
   get 'products/userproducts/:user_id/' => 'products#userproducts', :as => :userproducts
+  get 'products/admin_index/admin' => 'products#admin_index', :as => :admin_index
   get 'orders/myorders/:buyer_id' => 'orders#myorders', :as => :myorders
-    get 'orders/mysales/:seller_id' => 'orders#mysales', :as => :mysales
+  get 'orders/mysales/:seller_id' => 'orders#mysales', :as => :mysales
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
 end

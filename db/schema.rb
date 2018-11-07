60d436f7aa058b0f17a1ea4f1b6b16ff6cdf3995
sophia <sophia.zhu10@gmail.com>
2018-11-07 16:34:11 +1000

    add stripe key

--- db/schema.rb
+++ db/schema.rb
@@ -61,7 +61,6 @@ ActiveRecord::Schema.define(version: 2018_11_06_010220) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_products_on_user_id"
-  
   end
 
   create_table "roles", force: :cascade do |t|
